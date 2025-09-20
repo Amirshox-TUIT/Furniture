7bc0b48b83b451428f63e658a3a57b844ff81d10
Amirshox-TUIT <behruzfayzullayev3@gmail.com>
2025-09-20 11:26:32 +0500

    deploying done

--- core/settings.py
+++ core/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['167.172.110.210', 'furnitica-su05.onrender.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['46.101.235.40', 'furnitica-su05.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
@@ -163,3 +163,5 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = EMAIL
 EMAIL_HOST_PASSWORD = EMAIL_PASS
+
+
