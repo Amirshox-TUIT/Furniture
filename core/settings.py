bc3640b88392d63ef43fc83ab24ad184a8ee097d
Amirshox-TUIT <behruzfayzullayev3@gmail.com>
2025-09-20 09:49:58 +0500

    deploying

--- core/settings.py
+++ core/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['167.172.110.210', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['167.172.110.210', 'furnitica-su05.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
