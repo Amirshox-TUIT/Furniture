fd4011ea1aa142b945485439c8af9fb0ef471acd
Amirshox-TUIT <behruzfayzullayev3@gmail.com>
2025-09-20 10:15:03 +0500

    deploying

--- core/settings.py
+++ core/settings.py
@@ -84,15 +84,15 @@ WSGI_APPLICATION = 'core.wsgi.application'
 import os
 import dj_database_url
 DATABASES = {
-    'default': dj_database_url.config(default='postgresql://amir:gVadjhW5f5u7RVMdtP5Mio5rxp2KuUb0@dpg-d35tmbmr433s73aeg8n0-a/furniture_4mz3')
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.postgresql',
-    #     'NAME': DB_NAME,
-    #     'USER': DB_USER,
-    #     'PASSWORD': DB_PASS,
-    #     'HOST': DB_HOST,
-    #     'PORT': DB_PORT,
-    # }
+    # 'default': dj_database_url.config(default='postgresql://amir:gVadjhW5f5u7RVMdtP5Mio5rxp2KuUb0@dpg-d35tmbmr433s73aeg8n0-a/furniture_4mz3')
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': DB_NAME,
+        'USER': DB_USER,
+        'PASSWORD': DB_PASS,
+        'HOST': DB_HOST,
+        'PORT': DB_PORT,
+    }
 }
 
 
